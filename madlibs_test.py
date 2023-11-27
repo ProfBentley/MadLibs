d2c5582a136de42c4baa17014236dcff207b5b6a
ProfBentley <111934064+ProfBentley@users.noreply.github.com>
2023-11-27 16:21:49 -0500

    #000 First Commit
    This is an example description for a commit message. Extra description etc...

---
+++ madlibs_test.py
@@ -0,0 +1,15 @@
+'''
+This is an example
+Of a multi-line comment
+:)
+'''
+
+# get an adjective from the keyboard
+prompt1 = input("Give an adjective: ")
+
+# other code prompts here
+
+# print out the story line by line using the input answers
+print(f"It was a {prompt1} Fall night.")
+
+# fill out the rest...
